6840d106ca73baec1bd6cf681b44e35db00f9f64
Racheline <136965489+Rachel2001@users.noreply.github.com>
2023-08-25 18:54:17 +0200

    Update size.c

--- size.c
+++ size.c
@@ -2,7 +2,7 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * by Lamees and Racheline
+ * by Lamees and Racheline.
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
