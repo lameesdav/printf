dc2237e52d053a1214cadedacc3f9f5c28af5145
Racheline <136965489+Rachel2001@users.noreply.github.com>
2023-08-25 18:50:48 +0200

    Update flags.c

--- flags.c
+++ flags.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * by Lamees and Racheline
+ * by Lamees and Racheline.
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
