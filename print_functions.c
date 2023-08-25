40095638eb9673a5759ec1abbb9d260a93077b44
Racheline <136965489+Rachel2001@users.noreply.github.com>
2023-08-25 18:53:15 +0200

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -4,7 +4,7 @@
 
 /**
  * print_char - Prints a char
- * by Lamees and Racheline
+ * by Lamees and Racheline.
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
