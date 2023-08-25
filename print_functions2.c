522045c7577233575e614bf41e4191bb086d9629
Racheline <136965489+Rachel2001@users.noreply.github.com>
2023-08-25 18:53:56 +0200

    Update print_functions2.c

--- print_functions2.c
+++ print_functions2.c
@@ -3,7 +3,7 @@
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
- * by Lamees and Racheline
+ * by Lamees and Racheline.
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
