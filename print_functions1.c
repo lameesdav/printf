548ead8bb1c44bae0955c275f702a79ae363abd6
Racheline <136965489+Rachel2001@users.noreply.github.com>
2023-08-25 18:53:35 +0200

    Update print_functions1.c

--- print_functions1.c
+++ print_functions1.c
@@ -3,7 +3,7 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- * by Lamees and Racheline
+ * by Lamees and Racheline.
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
